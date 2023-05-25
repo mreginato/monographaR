30a2b915bcee78d72ac95d6be862e622f2b31d8d
mreginato <30278103+mreginato@users.noreply.github.com>
2023-05-25 16:08:19 -0300

    Update dataKey.R

--- R/dataKey.R
+++ R/dataKey.R
@@ -1,11 +1,27 @@
-dataKey <-
-function(dat, poly.sep="/", return.summary=T) {
+dataKey <- function(dat, poly.sep="/", return.summary=T) {
   colnames(dat)[1] <- "species"
   as.matrix(dat) -> dat
   dat.s <- apply(dat, MARGIN=2, FUN=function(x)(grep(poly.sep, x, fixed=T)))
   round(unlist(lapply(dat.s, length))/nrow(dat),2) -> dat.s
   dat.s[-1] -> dat.s
-  cat("Percentage of spp. with polymorphic characters per character:", fill=T)
+  cat("Percentage of spp. with polymorphic characters per character (initial):", fill=T)
+  cat(paste(names(dat.s), "=", dat.s), sep="\n")
+  ### Fix missing data
+  for (i in 2:ncol(dat)) {
+    dat[,i] -> d0
+    unique(sort(unlist(strsplit(d0, poly.sep, fixed=T)))) -> s0
+    paste(s0, collapse=poly.sep) -> s0
+    which(is.na(d0)) -> miss0
+    if (length(miss0) > 0) {
+      d0[miss0] <- s0
+    }
+    d0 -> dat[,i]
+  }
+  ###
+  dat.s <- apply(dat, MARGIN=2, FUN=function(x)(grep(poly.sep, x, fixed=T)))
+  round(unlist(lapply(dat.s, length))/nrow(dat),2) -> dat.s
+  dat.s[-1] -> dat.s
+  cat("Percentage of spp. with polymorphic characters per character (final):", fill=T)
   cat(paste(names(dat.s), "=", dat.s), sep="\n")
   
   matrix(ncol=ncol(dat), nrow=0) -> dat.n
@@ -16,7 +32,7 @@ function(dat, poly.sep="/", return.summary=T) {
     t(as.matrix(dat[match(sp0, dat[,1]),])) -> d0
     grep(poly.sep, d0) -> poly0
     if (length(poly0) > 0) {
-      strsplit(d0, poly.sep) -> d1
+      strsplit(d0, poly.sep, fixed=T) -> d1
       expand.grid(d1) -> dp
       colnames(dp) <- colnames(d0)
     } else {
